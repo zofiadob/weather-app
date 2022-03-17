aee3d0c82391e0566cc6532c1a0eec95e53f9eae
zofiadob <z.dobrowolska24@gmail.com>
2022-03-17 20:48:02 +0100

    quick setup

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import {Counter} from './features/Counter'
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">       
+        <Counter />
+      </header>
+    </div>
+  );
+}
+
+export default App;
